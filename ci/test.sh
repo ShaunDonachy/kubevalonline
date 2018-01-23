b1b73f59211e8b1cb14987b7386d8c32d8c2d234
Shaun <shaun.donachy@sojern.com>
2018-01-23 15:22:55 -0800

    add test stage to pipeline

--- ci/test.sh
+++ ci/test.sh
@@ -2,9 +2,16 @@
 
 set -e -x
 
-pushd kubevalonline
-    echo $GOPATH
-    echo `pwd`
-    mv kubevalonline $GOPATH/src/github.com/kubevalonline
-    make linux darwin
-popd
+mkdir -p $GOPATH/src/github.com/shaundonachy/
+cp -R ./kubevalonline $GOPATH/src/github.com/shaundonachy/.
+
+cd $GOPATH/src/github.com/shaundonachy/kubevalonline
+
+# RUN unit_tests and it shows the percentage coverage
+# print to stdout and file using tee
+make test | tee test_coverage.txt
+# add some whitespace to the begining of each line
+sed -i -e 's/^/     /' test_coverage.txt
+
+# Move to coverage-results directory.
+mv test_coverage.txt $GOPATH/coverage-results/.
